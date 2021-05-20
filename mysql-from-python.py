5f9fc229bd8d6e0b394e40d95e7402cf564b30cd
smcgdub <stephen_xyz1@hotmail.com>
2021-05-20 16:07:55 +0000

    push

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,9 +1,9 @@
 import os
+import datetime
 import pymysql
 
-# Get username from Cloud9 workspace
-# Modify this if running on another machine
-
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
 # Connect to the database
@@ -13,12 +13,17 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 
 try:
-    # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        # CODE BELOW IS TO UPDATE MANY ROWS
+        # rows = [("Bob", 21, "1990-02-06 23:04:56"),
+        #          ("Jim", 56, "1955-05-09 13:12:45"),
+        #          ("Fred", 100, "1911-09-12 01:01:01")]
+        # cursor.executemany("INSERT INTO Friends VALUES (%s, %s, %s);", rows)
+        # CODE BELOW IS TO UPDATE A ROW ALREADY CREATED
+        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'Bob';")
+        connection.commit()
+        # Note that the above will still display a warning (not error) if the
+        # table already exists
 finally:
-    # Close the connection regardeless of wheather the above was successfl
+    #Close the connection
     connection.close()
