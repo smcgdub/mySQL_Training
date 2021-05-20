739bb91443929f9bd5b0e84945aa4c32d3fdc218
smcgdub <stephen_xyz1@hotmail.com>
2021-05-20 12:33:03 +0000

    First save of day

---
+++ mysql-from-python.py
@@ -0,0 +1,24 @@
+import os
+import pymysql
+
+# Get username from Cloud9 workspace
+# Modify this if running on another machine
+
+username = os.getenv('C9_USER')
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
+
+try:
+    # Run a query
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    # Close the connection regardeless of wheather the above was successfl
+    connection.close()
